9d5567485b9f2dd0883d697b96156a7f83f7cdbe
Simone <smarras@njit.edu>
2023-04-09 21:20:08 -0400

    minor

--- src/io/plotting/jeplots.jl
+++ src/io/plotting/jeplots.jl
@@ -95,8 +95,8 @@ function plot_surf3d(SD::NSD_2D, mesh::St_mesh, q::Array, title::String, OUTPUT_
 
         #figure:
         fig = Figure(resolution=(1200, 400))
-        axs = [Axis3(fig[1, i]; aspect=(1, 1, 1), limits = (-1, 1, -1, 1, 0.5, 1.0)) for i = 1:1]
-        #axs = [Axis3(fig[1, i]; aspect=(1, 1, 1)) for i = 1:1]
+        #axs = [Axis3(fig[1, i]; aspect=(1, 1, 1), limits = (-1, 1, -1, 1, 0.5, 1.0)) for i = 1:1]
+        axs = [Axis3(fig[1, i]; aspect=(1, 1, 1)) for i = 1:1]
         
         hm = Makie.surface!(axs[1], xg, yg, zspl) # legend=:false, xl="x", yl="y", zl=string("q", ivar)) #, title=title, titlefont=12)
 
