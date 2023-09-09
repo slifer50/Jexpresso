ddc9487aefde12351d3a9ea3b6ea623c4ba695a9
smarras79 <smarras@njit.edu>
2023-09-09 15:57:13 -0400

    Pushing Inexact ready to go after partial addition of Exact

--- src/equations/CompEuler/theta/user_inputs.jl
+++ src/equations/CompEuler/theta/user_inputs.jl
@@ -3,16 +3,16 @@ function user_inputs()
         #---------------------------------------------------------------------------
         # User define your inputs below: the order doesn't matter
         #---------------------------------------------------------------------------
-        :tend                 => 10,
+        :tend                 => 100,
         :ode_solver           => SSPRK54(), #13.8s dt=0.35
-        :Δt                   => 0.2,
+        :Δt                   => 0.4,
         :ndiagnostics_outputs => 1,
         :case                 => "rtb",
         :lsource              => true,
         #---------------------------------------------------------------------------
         #Integration and quadrature properties
         #---------------------------------------------------------------------------
-        :lexact_integration  => true,
+        #:lexact_integration  => true,
         :interpolation_nodes =>"lgl",
         :nop                 => 4,      # Polynomial order
         :luser_bc            => true,
